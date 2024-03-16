ebf3c762c036867b346d52300579e02a012ce390
Gabriel Felix <pmvulgo@gmail.com>
2024-03-16 00:31:32 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,20 @@ import{ About, Contact, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas, Exper
 const App = () => {
   return (
     <BrowserRouter>
-    <div className="relative z-0 bg-primary">
-      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-      <Navbar/>
-      <Hero/>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div>
       </div>
-      <About/>
-      <Tech/>
-      <Works/>
-      <Feedbacks/>
-    </div>
-    <div className="relative z-0">
-    <Contact/>
-    <StarsCanvas/>
-    </div>
     </BrowserRouter>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,20 +6,32 @@ import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
+
   return (
-    <nav
-      className={
-        "${styles.paddindX} w-full items-center py-5 fixed top-0 z-20 bg-primary"
-      }
+    <nav>
+    <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+    <Link
+      to='/'
+      className='flex items-center gap-2'
+      onClick={() => {
+        setActive("");
+        window.scrollTo(0, 0);
+      }}
     >
-      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-        <Link
-          to="/"
-          className="flex items-center gap-2"
-          onClick={() => {
-            setActive("");
-          }}
-        ></Link>
+      <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+      <p className='text-white text-[18px] font-bold cursor-pointer flex '>
+        Gabriel &nbsp;
+        <span className='sm:block hidden'> | Ge</span>
+      </p>
+    </Link>
+    <ul className="list-none hidden sm:flex flex-row gap-10">
+      {navLinks.maps((Link) => (
+        <li>
+          <a href={`#${nav.id}`}></a>
+        </li>
+      ))}
+    </ul>
       </div>
     </nav>
   );
